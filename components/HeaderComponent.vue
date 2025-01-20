e952a12f7d90e58996f9534cf86c8f4eb88639cb
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-01-20 17:09:58 -0500

    creando proyecto

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="w-full sticky h-[10vh] pl-2 text-orange-500 pr-5 flex items-center justify-between">
+    <div class="flex gap-3 items-center">
+      <img class="h-[10vh]" src="../assets/images/Logo.png" alt="asdasd">
+      <h1 class="font-bold text-xl">GreedisGood</h1>
+    </div>
+    <div class="flex gap-10">
+      <a href="#">Productos</a>
+      <a href="#">Carrito</a>
+      <a href="#">Creditos</a>
+      <a href="#">Cuenta</a>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <HeaderComponent />
+  <section>
+    <div class="flex items-center h-[80vh] w-4/5 mt-3 mx-auto gap-10 justify-center">
+      <img class="size-[400px]" src="https://comprarmaquinariahosteleria.com/blog/wp-content/uploads/2023/02/como-montar-tienda-alimentacion.jpg" alt="asdadsasdasd">
+      <div class="flex flex-col gap-4 justify-between">
+        <h1 class="text-4xl font-Ysabeau">Descubre los Productos que Transformarán tu Vida</h1>
+        <p class="text-lg">En GreedisGood, transformamos la ambición en estilo. Ofrecemos una selección exclusiva de productos únicos, diseñados para quienes buscan lo mejor. Disfruta de una experiencia de compra superior con un proceso rápido y seguro, mientras te mantienes a la vanguardia de la innovación y el estilo.</p>
+        <strong class="text-lg">Nuestras Distinciones</strong>
+        <ul>
+          <li>Selección Exclusiva: Productos únicos y seleccionados para los más exigentes.</li>
+          <li>Experiencia de Compra Superior: Proceso rápido, seguro y enfocado en la satisfacción.</li>
+          <li>Innovación y Estilo: Lo último en tendencias y tecnología.</li>
+        </ul>
+        
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
